3c8be8e2971617c98993e414285f3f67f0ce9405
amin-abouee <amin.abouee@gmail.com>
2025-04-12 17:19:27 +0200

    Add formula for Librealsense version 2.50.0

---
+++ Formula/librealsense@2.50.0.rb
@@ -0,0 +1,56 @@
+class LibrealsenseAT2500 < Formula
+  desc "Intel RealSense D400 series and SR300 capture"
+  homepage "https://github.com/IntelRealSense/librealsense"
+  url "https://github.com/IntelRealSense/librealsense/archive/v2.50.0.tar.gz"
+  sha256 "cafeb2ed1efe5f42c4bd874296ce2860c7eebd15a9ce771f94580e0d0622098d"
+  license "Apache-2.0"
+  head "https://github.com/IntelRealSense/librealsense.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glfw"
+  depends_on "libusb"
+  depends_on "openssl@1.1"
+  # Build on Apple Silicon fails when generating Unix Makefiles.
+  # Ref: https://github.com/IntelRealSense/librealsense/issues/8090
+  on_arm do
+    depends_on xcode: :build
+  end
+
+  def install
+    ENV["OPENSSL_ROOT_DIR"] = Formula["openssl@1.1"].prefix
+
+    args = %W[
+      -DENABLE_CCACHE=OFF
+      -DBUILD_WITH_OPENMP=OFF
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+    if Hardware::CPU.arm?
+      args << "-DCMAKE_CONFIGURATION_TYPES=Release"
+      args << "-GXcode"
+    end
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <librealsense2/rs.h>
+      #include <stdio.h>
+      int main()
+      {
+        printf(RS2_API_VERSION_STR);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-o", "test"
+    assert_equal version.to_s, shell_output("./test").strip
+  end
+end
